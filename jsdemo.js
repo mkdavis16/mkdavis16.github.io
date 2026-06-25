2fc0c06cdbfa1f5dea65aa771dda00848db99a2b
mkdavis16 <marykatlindavis1@gmail.com>
2026-06-25 15:47:07 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Mary Katlin";
+let school = "West Charlotte HighSchool";
+var fun_fact = "I love classic cars";
+
+console.log(`Name: ${name}`);
+console.log(`School: ${name}`);
+console.log(`Fun Fact: ${fun_fact}`);
