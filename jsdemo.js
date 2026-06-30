c939824162b43b3e3c9f3916ee0d93ed3bc7f44e
mkdavis16 <marykatlindavis1@gmail.com>
2026-06-30 15:52:55 -0400

    Add HTML updates for displaying user information

--- jsdemo.js
+++ jsdemo.js
@@ -6,3 +6,7 @@ var fun_fact = "I love classic cars";
 console.log(`Name: ${name}`);
 console.log(`School: ${name}`);
 console.log(`Fun Fact: ${fun_fact}`);
+
+document.getElementById("name").innerHTML = "<b>Name: </b><i>" + name + "</i>";
+document.getElementById("school").innerHTML = "<b>School: </b><i>" + school + "</i>";
+document.getElementById("Fun_fact").innerHTML = "<b>Fun Fact: </b><i>" + fun fact  + "</i>";
