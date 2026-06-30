e5c51b482ad60d871188b774b606c83f0d867575
mkdavis16 <marykatlindavis1@gmail.com>
2026-06-30 16:02:19 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -4,9 +4,9 @@ let school = "West Charlotte HighSchool";
 var fun_fact = "I love classic cars";
 
 console.log(`Name: ${name}`);
-console.log(`School: ${name}`);
+console.log(`School: ${school}`);
 console.log(`Fun Fact: ${fun_fact}`);
 
 document.getElementById("Name").innerHTML = "<b>Name: </b><i>" + name + "</i>";
 document.getElementById("School").innerHTML = "<b>School: </b><i>" + school + "</i>";
-document.getElementById("Fun_fact").innerHTML = "<b>Fun Fact: </b><i>" + fun fact + "</i>";
+document.getElementById("Fun_fact").innerHTML = "<b>Fun Fact: </b><i>" + fun_fact + "</i>";
